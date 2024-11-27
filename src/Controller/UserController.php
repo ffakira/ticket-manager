f265aa9660bc0a53f953953f38c1ee925bc663a5
ffakira <55924290+ffakira@users.noreply.github.com>
2024-11-27 13:21:50 -0300

    impl: boilerplate user controller for login and register views

---
+++ src/Controller/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\LoginType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController {
+
+    #[Route('/login', name: 'login', methods: ['GET', 'POST'])]
+    public function login(Request $request, UserRepository $userRepository) {
+        $user = new User();
+
+        $form = $this->createForm(LoginType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /**
+             * @TODO missing implementation & validation
+             */
+        }
+
+        return $this->render('login.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/register', name: 'register', methods: ['GET', 'POST'])]
+    public function register(Request $request) {
+        $user = new User();
+
+        return $this->render('register.html.twig');
+    }
+}
